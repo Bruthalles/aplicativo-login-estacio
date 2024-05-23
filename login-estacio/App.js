313c61885a4cbcd2b038bf15408b5372e4420508
thalles_rcb <thallesgmkr@gmail.com>
2024-05-22 22:01:35 -0300

    erro de chave aberta sem sentido

--- login-estacio/App.js
+++ login-estacio/App.js
@@ -4,6 +4,6 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 export default function App(){
     <View>
         <Text>hello world :)</Text>
-        {<Image source={require('')}></Image>
+        <Image source={require('')}></Image>
     </View>
 }
